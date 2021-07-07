be547c86d75ac228c9690eae6bae46f7e4a391bb
Сергей Коненков <konenkovsa@mail.ru>
2021-07-07 11:06:26 +0500

    HW05 rev.03.1

--- yatube/about/tests/test_views.py
+++ yatube/about/tests/test_views.py
@@ -1,6 +1,8 @@
 from django.test import Client, TestCase
 from django.urls import reverse
 
+from http import HTTPStatus
+
 
 class StaticViewsTests(TestCase):
     def setUp(self):
@@ -9,7 +11,7 @@ class StaticViewsTests(TestCase):
     def test_author_page_accessible_by_name(self):
         """URL, генерируемый при помощи имени 'about:author', доступен."""
         response = self.guest_client.get(reverse('about:author'))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_author_page_uses_correct_template(self):
         """При запросе к about:author
@@ -20,7 +22,7 @@ class StaticViewsTests(TestCase):
     def test_tech_page_accessible_by_name(self):
         """URL, генерируемый при помощи имени about:tech, доступен."""
         response = self.guest_client.get(reverse('about:tech'))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_tech_page_uses_correct_template(self):
         """При запросе к about:tech
